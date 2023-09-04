a4b6602be127bd7ecb4ec545cef5d179cda089c7
eroberts01 <01ethanroberts@gmail.com>
2023-09-03 23:12:57 -0400

    add answer feedback component

--- quizapp/src/components/QuestionShuffler.js
+++ quizapp/src/components/QuestionShuffler.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import questions_file from './../questions.json';
+import { AnswerFeedback } from './AnswerFeedback';
 
 // taken from https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
 const shuffle = (array) => {
@@ -10,7 +11,7 @@ export function QuestionShuffler() {
     let shuffled_questions = shuffle(questions_file["Test quiz"]["questions"]);
     return (
         <div>
-            {shuffled_questions.map((q) => <div>{q.question}</div>)}
+            {shuffled_questions.map((q) => <AnswerFeedback question={q} answer="A"/>)}
         </div>
     );
 }
