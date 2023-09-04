a6df64cb8c5d7dec9b60d6cee74ea8ac6cdf5826
eroberts01 <01ethanroberts@gmail.com>
2023-09-03 20:22:17 -0400

    create questions json and QuestionShuffler

---
+++ quizapp/src/components/QuestionShuffler.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import questions_file from './../questions.json';
+
+// taken from https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
+const shuffle = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+}
+
+export function QuestionShuffler() {
+    let shuffled_questions = shuffle(questions_file["Test quiz"]["questions"]);
+    return (
+        <div>
+            {shuffled_questions.map((q) => <div>{q.question}</div>)}
+        </div>
+    );
+}
