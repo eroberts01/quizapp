9d7a5d9931f5aa9fa2d3bd4d5b4cb7d461648d85
eroberts01 <01ethanroberts@gmail.com>
2023-09-08 21:17:31 -0400

    clean up code, add support for fill in the blank

--- quizapp/src/components/QuizForm.js
+++ quizapp/src/components/QuizForm.js
@@ -17,34 +17,34 @@ export default function QuizForm() {
     useEffect(() => {
         setQuestions(shuffle(questions_file[quizId]["questions"]));
     }, []);
-    const [answers, setAnswers] = useState([]);
+    const [answers, setAnswers] = useState({});
     const [data, setData] = useState({passedQuestions: [], quizResults: [], isVisible: false});
-    return (
+        return (
         !data.isVisible ? 
             <Form className='p-3 col-6' onSubmit={(e) => {
                 e.preventDefault();
                 setData({passedQuestions: shuffled_questions,
-                    quizResults: Array.from(answers.map(ans => ans.question)), isVisible: true});
+                    quizResults: Array.from(Object.values(answers).map(ans => ans.answer)), isVisible: true});
             }}>
                 <h3 className='mb-5'>Questions:</h3>
                 {shuffled_questions.map((q, id) =>
                     <div className='question'>
                         <p>{id + 1}. {q.question}</p>
-                            {Object.keys(q.answer_choices).map((a) =>
+                            {q.type === "multiple_choice" || q.type === "true_false" ?
+                                Object.keys(q.answer_choices).map((a) =>
                                 <div className='answerChoices'>
                                     <input type="radio" name={id} className='p-2' id={a + '_' + id} value={q.answer_choices[a]} 
                                     onChange={(e) => {
-                                        let answerId = answers.findIndex((ans) => ans.question == id);
-                                        if(answerId === -1){
-                                            answers.push({question: a + "_" + id, answer: e.target.value});
-                                        }
-                                        else {
-                                            answers[answerId] = {question: a + "_" + id, answer: e.target.value};
-                                        }
+                                        answers[id] = {question: a + "_" + id, answer: e.target.value};
                                         setAnswers(answers);
                                     }}/>
                                     <label htmlFor={a + '_' + id} className='p-2'>{q.answer_choices[a]}</label>
                                 </div>)
+                                : <input type="text" name={id} className='p-2' id={id}
+                                onChange={(e) => {
+                                    answers[id] = {question: id, answer: e.target.value};
+                                    setAnswers(answers);
+                                }}/>
                             }
                     </div>)
                 }
