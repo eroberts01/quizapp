b70acf3a1f71c79400d89f776a1561b37384c3d5
Mani9732 <mani.shah23@gmail.com>
2023-09-22 16:28:04 -0400

    Fixed 4.5 feedback Issues. Details in Google Docs

--- quizapp/src/components/QuizRow.js
+++ quizapp/src/components/QuizRow.js
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 import questions_file from './../questions.json';
 import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 
 export default function QuizRow({ searchVal }) {
     const [questions, setQuestions] = useState(Object.entries(questions_file));
     useEffect(() => {
         if (searchVal) {
             setQuestions(Object.entries(questions_file).filter(([id, quiz]) => quiz.name.toLowerCase().includes(searchVal.toLowerCase())));
+        }else if(searchVal.length === 0){
+            setQuestions(Object.entries(questions_file))
         }
-
     }, [searchVal]);
+
+    function resetQuestions() {
+        setQuestions(Object.entries(questions_file))
+      }
 
     return (
         <div className="row container-fluid mt-5">
@@ -18,13 +24,12 @@ export default function QuizRow({ searchVal }) {
                     <div className="card">
                         <div className="card-body">
                             <h5 className="card-title">No Quizzes Found!</h5>
-                            {/* LINK BACK TO HOMEPAGE WITH ALL QUIZZES NOT WORKING */}
-                            <Link to='/' className="btn btn-primary mt-4"> 
+                            <Button className="btn btn-primary mt-4" onClick={resetQuestions}> 
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
                                 </svg>
                                 Home
-                            </Link>
+                            </Button>
                         </div>
                     </div>
                 </div>
