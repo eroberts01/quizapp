837e1404c0fb99394eb731da1decea8ad4d0e80b
Mani9732 <mani.shah23@gmail.com>
2023-09-22 15:59:43 -0400

    Fixed 4.5 feedback issues. Details in Google Docs

--- quizapp/src/components/QuizRow.js
+++ quizapp/src/components/QuizRow.js
@@ -18,7 +18,8 @@ export default function QuizRow({ searchVal }) {
                     <div className="card">
                         <div className="card-body">
                             <h5 className="card-title">No Quizzes Found!</h5>
-                            <Link to='/' className="btn btn-primary mt-4">
+                            {/* LINK BACK TO HOMEPAGE WITH ALL QUIZZES NOT WORKING */}
+                            <Link to='/' className="btn btn-primary mt-4"> 
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
                                 </svg>
