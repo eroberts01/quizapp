014ea87db98ab63b87914f198b4051841b38dd20
Mani9732 <mani.shah23@gmail.com>
2023-10-26 12:31:56 -0400

    Fixed Issues Format, spacing, clarify results pgs

--- quizapp/src/components/QuizForm.js
+++ quizapp/src/components/QuizForm.js
@@ -20,17 +20,17 @@ export default function QuizForm(props) {
         setQuestions(shuffle(questions_file[quizId]["questions"]));
     }, []);
     const [answers, setAnswers] = useState({});
-    const [data, setData] = useState({ passedQuestions: [], quizResults: [], isVisible: false, onSetSearchBarVisible: false});
+    const [data, setData] = useState({ passedQuestions: [], quizResults: [], isVisible: false, onSetSearchBarVisible: false });
     const [answeredSoFar, setAnsweredSoFar] = useState(0);
     const validate = () => {
         return answeredSoFar >= Array.from(Object.values(shuffled_questions)).length;
     };
     return (
         !data.isVisible ?
-            <Form className='p-3' onSubmit={(e) => {
+            <Form className='p-3 col-6' onSubmit={(e) => {
                 e.preventDefault();
                 const answer = window.confirm("Are you sure you would like to submit the quiz?");
-                if(!answer) {
+                if (!answer) {
                     return;
                 }
                 props.onSetSearchBarVisible(false);
@@ -39,41 +39,47 @@ export default function QuizForm(props) {
                     quizResults: Array.from(Object.values(answers).map(ans => ans.answer)), isVisible: true
                 });
             }}>
-                 {!validate() ? 
-                <h5 key={answeredSoFar} className='alert alert-info text-wrap w-25 text-center' style={{'position': 'fixed', 'bottom': 0, 'right': 0}}>
-                    <h6>QUESTIONS LEFT:</h6>
-                    {[...Array(shuffled_questions.length).keys()].filter((id) => !Object.keys(answers).includes(id.toString())).map(x=>x+1).join(", ")}
-                    </h5> 
-                : ""}
+                {!validate() ?
+                    <h5 key={answeredSoFar} className='alert alert-info text-wrap w-25 text-center' style={{ 'position': 'fixed', 'bottom': 0, 'right': 0 }}>
+                        <h6>QUESTIONS LEFT:</h6>
+                        {[...Array(shuffled_questions.length).keys()].filter((id) => !Object.keys(answers).includes(id.toString())).map(x => x + 1).join(", ")}
+                    </h5>
+                    : ""}
                 <h2 className='mb-5 text-center'>{questions_file[quizId]["name"]}</h2>
                 <h3 className='mb-5'>Questions:</h3>
                 {shuffled_questions.map((q, id) =>
-                    <div className='question'>
-                        <p>{id + 1}. {q.question}</p>
-                        {q.type === "multiple_choice" || q.type === "true_false" ?
-                            Object.keys(q.answer_choices).map((a) =>
-                                <div className='answerChoices'>
-                                    <input type="radio" name={id} className='p-2' id={a + '_' + id} value={q.answer_choices[a]}
-                                        onChange={(e) => {
-                                            answers[id] = { question: a + "_" + id, answer: e.target.value };
-                                            setAnswers(answers);
-                                            setAnsweredSoFar(Array.from(Object.values(answers)).length);
-                                        }} />
-                                    <label htmlFor={a + '_' + id} className='p-2'>{q.answer_choices[a]}</label>
-                                </div>)
-                            :
-                            <input className="form-control" name={id} id={id} placeholder='answer'
-                                onChange={(e) => {
-                                    answers[id] = { question: id, answer: e.target.value.toLowerCase() };
-                                    setAnswers(answers);
-                                    setAnsweredSoFar(Array.from(Object.values(answers)).length)
-                                }} />
-                        }
-                    </div>)
+                    <>
+                        <div className='question card pt-3 pb-3 ps-3 pe-3' style={{ 'text-align': 'left', 'align-content': 'left', 'background-color':'#434a58'}}>
+                        <h6 class="card-header">{id + 1}. {q.question}</h6>
+                            {/* <p>{id + 1}. {q.question}</p> */}
+                            {q.type === "multiple_choice" || q.type === "true_false" ?
+                                Object.keys(q.answer_choices).map((a) =>
+                                    <div className='answerChoices'>
+                                        <input type="radio" name={id} className='p-2' id={a + '_' + id} value={q.answer_choices[a]}
+                                            onChange={(e) => {
+                                                answers[id] = { question: a + "_" + id, answer: e.target.value };
+                                                setAnswers(answers);
+                                                setAnsweredSoFar(Array.from(Object.values(answers)).length);
+                                            }} />
+                                        <label htmlFor={a + '_' + id} className='p-2'>{q.answer_choices[a]}</label>
+                                    </div>)
+                                :
+                                <input className="form-control" name={id} id={id} placeholder='answer'
+                                    onChange={(e) => {
+                                        answers[id] = { question: id, answer: e.target.value.toLowerCase() };
+                                        setAnswers(answers);
+                                        setAnsweredSoFar(Array.from(Object.values(answers)).length)
+                                    }} />
+                            }
+                        </div>
+                        <br></br>
+                        <br></br>
+                    </>
+                )
                 }
-               
+
                 <Button type="submit" className='btn d-block btn-primary mt-3' disabled={!validate()}>Submit</Button>
             </Form> :
-            <AnswerFeedback data={data}/>
+            <AnswerFeedback data={data} />
     );
 }
